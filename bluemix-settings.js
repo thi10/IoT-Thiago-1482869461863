9ffe3ced059b155b6e0a6c9cc7e528fc5f72259d
thi10 <thiagolima@gmail.com>
2016-12-29 13:28:20 -0300

    Update bluemix-settings.js

--- bluemix-settings.js
+++ bluemix-settings.js
@@ -52,7 +52,7 @@ var settings = module.exports = {
     httpStatic: path.join(__dirname,"public"),
 
     functionGlobalContext: { 
-        crc:require('crc');
+        crc:require('crc')
     },
 
     storageModule: require("./couchstorage")
